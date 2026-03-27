ff280e855754b9c40f96d043640fa204ac14c5dc
user-mings <3132225671@qq.com>
2026-03-27 23:33:54 +0800

    Add files via upload

--- 02.常见数据类型.py
+++ 02.常见数据类型.py
@@ -1,21 +1,48 @@
-#type() ---> 获取指定的字面量或变量的数据类型
-from types import NoneType
+# #type() ---> 获取指定的字面量或变量的数据类型
+# from types import NoneType
+#
+# print("Hello Python")
+#
+# print(type("Hello"))#str
+# print(type(30))#int
+# print(type(3.14))#float
+# print(type(True))#bool
+# print(type(False))#bool
+# print(type(None))#NoneType
+#
+# num = 50
+# print(type(num))#int
+#
+# #isinstance(数据,类型) ----> bool值 ----> 判定数据是否是指定的类型, 是:True , 否:False
+# print(isinstance(num,int))#True
+# print(isinstance(num,bool))#False
+# print(isinstance(num,str))#False
+# print(isinstance(num,float))#False
+# print(isinstance(num,NoneType))#False
 
-print("Hello Python")
+#字符串
+#定义字符串的三种方式
+s1 = "Hello"#双引号定义
+s2 = 'Python'#单引号定义
+s3 = """
+Hello:
+    欢迎大家进行Python的学习
+    请多关注
+"""#三引号定义
 
-print(type("Hello"))#str
-print(type(30))#int
-print(type(3.14))#float
-print(type(True))#bool
-print(type(False))#bool
-print(type(None))#NoneType
+print(s1)
+print(s2)
+print(s3)
 
-num = 50
-print(type(num))#int
+#\'表示单引号  \"表示双引号 \n换行符 \t制表符
+mgs = 'It\'s very interesting'
+print(mgs)
 
-#isinstance(数据,类型) ----> bool值 ----> 判定数据是否是指定的类型, 是:True , 否:False
-print(isinstance(num,int))#True
-print(isinstance(num,bool))#False
-print(isinstance(num,str))#False
-print(isinstance(num,float))#False
-print(isinstance(num,NoneType))#False
+msg2 = "It's very interesting"
+print(msg2)
+
+msg3 = "\"It's very good\""
+print(msg3)
+
+mgs4 = """Hello:\n\tIt's very interesting"""
+print(mgs4)
